5dff5ef271175a95fb82204f4e24c560db335c31
rutikagediya <165748540+rutikagediya@users.noreply.github.com>
2024-04-06 10:11:30 +0700

    Add files via upload

---
+++ 38.php
@@ -0,0 +1,44 @@
+<html>
+
+<body>
+
+<form action="upload.php"
+
+method="post" enctype="multipart/form-data">
+
+Select File:
+
+<input type="file" name="fileToUpload"/>
+
+<input type="submit" value="Upload" Image"
+
+name="submit"/>
+
+</form>
+
+</body>
+
+</html>
+<?php
+
+$target_path="d:/";
+
+$target_path=$target_path.basename($_FILES['fileToUpload']['n ame']);
+
+if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path))
+
+{
+
+echo "File uploaded successfully!";
+
+}
+
+else
+
+{
+
+echo "Sorry, file not uploaded, please try again!";
+
+}
+
+?>
